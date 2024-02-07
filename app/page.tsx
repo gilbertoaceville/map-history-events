4011dbf222c49fadd8e4f7b7dadbddc86d80833a
gilbertoaceville <gilbertoaceville@gmail.com>
2024-02-07 12:54:33 +0100

    fix: use dynamic imports to fix window not defined error

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,11 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+const Map = dynamic(() => import("./components/map"), { ssr: false });
 
 export default function Home() {
   return (
-    <main className="content-wrapper">
-      <div className="flex flex-col w-full h-full">
-        <div className="h-12"></div>
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-          className="map-container"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
+    <main className="h-full w-full">
+      <Map />
     </main>
   );
 }
